a7044ba9fbd4c3e330d6bd939da808a35861f06e
Felipe Arce <arceprogramando@gmail.com>
2023-05-30 18:51:55 -0300

    armado de carpetas

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import { Server } from 'socket.io'
+import handlebars from 'express-handlebars'
+import viewsRouter from './routes/views.router.js'
+import __dirname from "./utils.js";
+
+
+const app = express()
+
+app.use(express.static(`${__dirname}/public`));
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'handlebars')
+
+const PORT = 8080
+
+const server = app.listen(PORT, () => console.log(`server on in port http://localhost:${PORT}`))
+
+const io = new Server(server)
+
