d2bd93c1aa7ed7fb67fc22e33a00b56baf733bc8
arceprog <arceprogramando@gmail.com>
2023-07-03 23:16:58 -0300

    Agregado form funcional front con handlebar , envio de post

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,11 +1,58 @@
-/* eslint-disable object-curly-newline */
-/* eslint-disable operator-linebreak */
+// CRUD products Routes
 import { Router } from 'express';
 import ProductManager from '../ProductManager.js';
+import uploadMiddleware from '../services/uploader.js';
 
 const router = Router();
 
 const productManager = new ProductManager('./src/files/products.json');
+
+// Creacion Create
+
+router.post('/api/products', uploadMiddleware, async (req, res) => {
+  try {
+    const {
+      title, description, code, price, status, stock, category,
+    } = req.body;
+
+    let thumbnails = null;
+    if (req.file) {
+      thumbnails = `/upload/${req.file.filename}`;
+    }
+
+    if (!title
+      || !description
+      || !code
+      || !price
+      || !status
+      || !stock
+      || !category
+      || !thumbnails) {
+      return res.status(400).json({
+        error: 'Todos los campos son requeridos',
+      });
+    }
+
+    const product = {
+      title,
+      description,
+      code,
+      price,
+      status,
+      stock,
+      category,
+      thumbnails,
+    };
+
+    const createdProduct = await productManager.writeProduct(product);
+
+    return res.status(201).json({ status: 'success', product: createdProduct });
+  } catch (error) {
+    return res.status(500).json({ error: `Error al crear el producto ${error}` });
+  }
+});
+
+// Lectura Read
 
 router.get('/api/products', async (req, res) => {
   try {
@@ -23,26 +70,10 @@ router.get('/api/products', async (req, res) => {
   }
 });
 
-router.get('/', async (req, res) => {
-  try {
-    const products = await productManager.getProducts();
-    // eslint-disable-next-line no-console
-    // console.log(products);
-    const PORT = 8080;
-    res.render('home', {
-      products,
-      style: 'index.css',
-      port: PORT,
-    });
-  } catch (error) {
-    res.status(500).json({ error: 'Error al obtener los productos' });
-  }
-});
-
 router.get('/api/products/:pid', async (req, res) => {
   try {
     const { pid } = req.params;
-    const product = await productManager.getProductById(pid);
+    const product = await productManager.getProductsById(pid);
 
     if (product) {
       res.status(200).json(product);
@@ -56,50 +87,67 @@ router.get('/api/products/:pid', async (req, res) => {
   }
 });
 
-// eslint-disable-next-line consistent-return
-router.put('/api/products/:pid', async (req, res) => {
+// Actulizacion Update
+
+router.put('/api/products/:pid', uploadMiddleware, async (req, res) => {
   try {
     const { pid } = req.params;
-    const { title, description, code, price, stock, category, thumbnails } =
-      req.body;
+    const {
+      title, description, code, price, status, stock, category,
+    } = req.body;
+
+    let thumbnails = null;
+    if (req.file) {
+      thumbnails = `/src/public/upload/${req.file.filename}`;
+    }
 
     if (
-      !title &&
-      !description &&
-      !code &&
-      !price &&
-      !stock &&
-      !category &&
-      !thumbnails
+      !title
+      || !description
+      || !code
+      || !price
+      || !status
+      || !stock
+      || !category
+      || !thumbnails
     ) {
       return res.status(400).json({
-        error: 'Se debe proporcionar al menos un campo para actualizar',
+        error: 'Todos los campos son requeridos',
       });
     }
 
-    const product = await productManager.getProductById(pid);
+    const updatedProduct = await productManager.updateProduct(pid, {
+      title,
+      description,
+      code,
+      price,
+      status,
+      stock,
+      category,
+      thumbnails,
+    });
+
+    return res.status(200).json({ status: 'success', product: updatedProduct });
+  } catch (error) {
+    return res.status(500).json({ error: `Error al actualizar el producto ${error}` });
+  }
+});
+// Eliminación Delete
+
+router.delete('/api/products/:pid', async (req, res) => {
+  try {
+    const { pid } = req.params;
+    const product = await productManager.getProductsById(pid);
 
     if (!product) {
       return res.status(404).json({ error: 'El producto no existe' });
     }
 
-    const updatedProductData = {
-      title: title || product.title,
-      description: description || product.description,
-      code: code || product.code,
-      price: price || product.price,
-      stock: stock || product.stock,
-      category: category || product.category,
-      thumbnails: thumbnails || product.thumbnails,
-    };
+    await productManager.deleteProduct(pid);
 
-    const updatedProduct = await productManager.updateProduct(
-      pid,
-      updatedProductData,
-    );
-    res.status(200).json({ status: 'success', product: updatedProduct });
+    return res.status(200).json({ status: 'success', message: 'Producto eliminado correctamente' });
   } catch (error) {
-    res.status(500).json({ error: 'Error al actualizar el producto' });
+    return res.status(500).json({ error: `error al eliminar el producto ${error}` });
   }
 });
 
